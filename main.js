a02749dd35a3c6e07ffef73f57726273b4826066
jinchuu1391 <62422486+jinchuu1391@users.noreply.github.com>
2020-11-26 17:40:35 +0900

    버튼에 각 페이지로 스크롤 이벤트 추가

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (!link) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+const contactMeBtn = document.querySelector(".home__contact");
+contactMeBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}

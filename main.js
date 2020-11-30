6f2d9debb230730ca16f08876725d64e864f794e
jinchuu1391 <62422486+jinchuu1391@users.noreply.github.com>
2020-11-30 15:45:05 +0900

    스크롤 되면 home 투명하게 되는 효과 추가

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+document.addEventListener("scroll", () => {});
+
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
@@ -13,6 +15,11 @@ navbarMenu.addEventListener("click", (event) => {
 const contactMeBtn = document.querySelector(".home__contact");
 contactMeBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
+});
+
+const home = document.querySelector("#home");
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / home.getBoundingClientRect().height;
 });
 
 function scrollIntoView(selector) {
